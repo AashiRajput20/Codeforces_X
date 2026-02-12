0ce27ac901fa2f01d85950cb9a34bb567880c5d0
Aashi Rajput <aashi.rajput.245@gmail.com>
2026-02-12 13:17:41 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-black text-white h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-cyan-400">
+        Codeforces X 🚀
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
